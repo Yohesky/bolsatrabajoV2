92aeb9ee4f6e6b31926172e35546763889f72788
Yohesky <yoheskyjpp@gmail.com>
2019-10-16 08:16:36 -0400

    se agrego descripcionEmpresa.php

--- descripcionEmpresa.php
+++ descripcionEmpresa.php
@@ -19,7 +19,18 @@ while($row = mysqli_fetch_array($resultado))
 {
     echo
     "
-        <h1>'".$row['descripcionEmpresa']."'</h1>
+    <div class='card text-center mt-5'>
+    <div class='card-header'>
+    <h3> ".$row['nombreEmpresa']." </h3>
+    </div>
+    <div class='card-body'>
+      <h5 class='card-title'> ".$row['descripcionEmpresa']." </h5>
+      <p class='card-text'> ".$row['rif']." </p>
+      <p class='card-text'> ".$row['direccionEmpresa']." </p>
+      <p class='card-text'> ".$row['areaEmpresa']." </p>
+      <p class='card-text'> ".$row['correoEmpresa']." </p>
+      <p class='card-text'> ".$row['webEmpresa']." </p>
+    </div>
     ";
 }
 
