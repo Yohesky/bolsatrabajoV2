0022b6f409d0bf7f81e3252da8e02d9ca1dc9cf7
Yohesky <yoheskyjpp@gmail.com>
2019-12-08 22:31:46 -0400

    cambios arreglados

--- includes/filtroUsuario.php
+++ includes/filtroUsuario.php
@@ -8,15 +8,16 @@ include("conexion.php");
 $titulo = $_POST["titulo"];
 $sueldo = $_POST["sueldo"];
 $localizacion = $_POST["localizacion"];
+$categoria = $_POST["categoria"];
 
 
 //si el valor de busqueda NO esta vacio se hace una consulta
-if(isset($_POST["titulo"]) && isset($_POST["sueldo"]))
+if(isset($_POST["titulo"]) && isset($_POST["sueldo"]) && isset($_POST["localizacion"]) && isset($_POST["categoria"]))
 {
     //consulta a la base de datos
     //selecciona todos los datos de la tabla llamada TAREAS
     //donde el nombre de la tarea coincida con la variable llamada BUSQUEDA
-    $query = "SELECT * FROM propuesta WHERE titulo LIKE '$titulo%' AND sueldo LIKE '$sueldo%' AND localizacion='$localizacion' ";
+    $query = "SELECT * FROM propuesta JOIN empresa ON propuesta.empresa_idempresa = empresa.idempresa WHERE titulo LIKE '$titulo%' AND sueldo LIKE '$sueldo%' AND localizacion LIKE '$localizacion%' AND categoria LIKE '$categoria%' ";
     $result = mysqli_query($conexion, $query);
 
     //si no tengo respuesta de la BD
@@ -37,7 +38,8 @@ if(isset($_POST["titulo"]) && isset($_POST["sueldo"]))
             "sueldo" => $row ["sueldo"],
             "descripcion" => $row ["descripcion"],
             "funciones" => $row["funciones"],
-            "idpropuesta" => $row ["idpropuesta"]
+            "idpropuesta" => $row ["idpropuesta"],
+            "idempresa" => $row["idempresa"]
         );
     }
 
