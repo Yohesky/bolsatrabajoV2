26abf068f1b8a170ac216c7d14880ff799837cbb
José Aljuria <josejavieral13@gmail.com>
2020-02-05 22:50:57 -0400

    listo los perfiles de los trabajadores

--- perfil.php
+++ perfil.php
@@ -1,7 +1,9 @@
 <?php include("includes/headerTrabajador.php") ?>
 
-<main class="container shadow-lg" id="infoTrabajador">
+<main class="container" >
+	<div id="infoTrabajador" style="max-width: 760px;" class="mx-auto shadow-lg">
 
+	</div>
 </main>
 
 <?php include("includes/footer.php") ?>
