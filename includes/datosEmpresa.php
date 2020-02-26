92cf7c66323fa3d7fe1d4292bc6a44c4a43bf935
José Aljuria <josejavieral13@gmail.com>
2020-02-25 21:32:18 -0400

    listo validaciones de las empresas

--- includes/datosEmpresa.php
+++ includes/datosEmpresa.php
@@ -21,20 +21,48 @@ while($row = mysqli_fetch_array($resultado))
    "
    <form id='formularioActualizacion' class='needs-validation' novalidate  >
    <div class='form-group'>
-                            <input type='text' id='nombreEmpresa' name='nombreEmpresa' value='".$row['nombreEmpresa']."' class='form-control' required maxlength='50'>
+                            <input type='text' id='nombreEmpresa' name='nombreEmpresa' value='".$row['nombreEmpresa']."' class='form-control' required maxlength='50' placeholder='Nombre de la empresa'>
     </div>
 
     <div class='form-group'>
-                            <input type='text' id='rif' name='rif' value='".$row['rif']."' class='form-control' required maxlength='12'>
+                            <input type='text' id='rif' name='rif' value='".$row['rif']."' class='form-control' required maxlength='12' placeholder='Rif de la empresa'>
     </div>
 
     <div class='form-group'>
-                            <input type='text' id='direccionEmpresa' name='direccionEmpresa' value='".$row['direccionEmpresa']."' class='form-control' required maxlenth='100'>
+                            <input type='text' id='direccionEmpresa' name='direccionEmpresa' value='".$row['direccionEmpresa']."' class='form-control' required maxlenth='100' placeholder='Dirección de la empresa'>
     </div>
 
+
     <div class='form-group'>
-                            <input type='text' id='areaEmpresa' name='areaEmpresa' value='".$row['areaEmpresa']."' class='form-control' required maxlength='50'>
-    </div>
+                    <select id='areaEmpresa' name='areaEmpresa' class='form-control' required>
+                    <option value='".$row['areaEmpresa']."' >".$row['areaEmpresa']."</option>
+                        <option value='Administración/Oficinas'>Administración/Oficinas</option>
+                        <option value='Almacén/Logística/Trasporte'>Almacén/Logística/Trasporte</option>
+                        <option value='Atención al Cliente'>Atención al Cliente</option>
+                        <option value='Servicios generales, aseo y seguridad'>Servicios generales, aseo y seguridad</option>
+                        <option value='CallCenter/Telemercadeo'>CallCenter/Telemercadeo</option>
+                        <option value='Producción/Operarios'>Producción/Operarios</option>
+                        <option value='Manufactura'>Manufactura</option>
+                        <option value='Medicina/Saldud'>Medicina/Saldud</option>
+                        <option value='Comunicación'>Comunicación</option>
+                        <option value='Construcción y Obras'>Construcción y Obras</option>
+                        <option value='Contabilidad/Finanzas'>Contabilidad/Finanzas</option>
+                        <option value='Mercadotecnía/Publicidad'>Mercadotecnía/Publicidad</option>
+                        <option value='Diseño/Artes Gráficas'>Diseño/Artes Gráficas</option>
+                        <option value='Docencia'>Docencia</option>
+                        <option value='Compras/Comercio Exterior'>Compras/Comercio Exterior</option>
+                        <option value='Dirección/Gerencía'>Dirección/Gerencía</option>
+                        <option value='Técnicas'>Técnicas</option>
+                        <option value='Investigación y Calidad'>Investigación y Calidad</option>
+                        <option value='Hostelería/Turismo'>Hostelería/Turismo</option>
+                        <option value='Informatica/Telecomunicaciones'>Informática/Telecomunicaciones</option>
+                        <option value='Ingeniería'>Ingeniería</option>
+                        <option value='Legal/Asesorias'>Legal/Asesorias</option>
+                        <option value='Mantenimiento y Reparaciones'>Mantenimiento y Reparaciones</option>
+                        <option value='Recursos Humanos'>Recursos Humanos</option>
+                        <option value='Ventas'>Ventas</option>
+                    </select>
+                </div>
 
     <div class='form-group'>
                             <input type='email' id='correoEmpresa' name='correoEmpresa' value='".$row['correoEmpresa']."' class='form-control' required maxlength='45'>
@@ -42,7 +70,6 @@ while($row = mysqli_fetch_array($resultado))
 
     <div class='form-group'>
                         <select name='estado' id='estado' class='form-control'>
-                            <option value=' disabled selected>Selecciona tu estado</option>
                             <option value='".$row['estado']."'>".$row['estado']."</option>
                             <option value='Amazonas'>Amazonas</option>
                             <option value='Anzoategui'>Anzoategui</option>
--- js/perfilEmpresa.js
+++ js/perfilEmpresa.js
@@ -10,9 +10,11 @@ $(function () {
       
       e.preventDefault();
 
-        let form = $("#formularioActualizacion").serialize();
-        console.log(form)
-        if ($.trim($('#nombreEmpresa').val()).length > 0) {
+        const formulario = document.getElementById('formularioActualizacion');
+        
+        if (formulario.checkValidity()) {
+
+          let form = $("#formularioActualizacion").serialize();console.log(form);
           $.ajax({
             type: 'POST',
             url: 'includes/actDatosEmpresa.php',
@@ -817,14 +819,14 @@ $(function () {
 
             default:
                 $("#ciudad").empty();
-                $("#ciudad").append("<option>--Select--</option>");
+                $("#ciudad").append("<option value=''>--Select--</option>");
                 break;
         }
     });
 
     function city(arr) {
         $("#ciudad").empty();//To reset cities
-        $("#ciudad").append("<option>--Select--</option>");
+        $("#ciudad").append("<option value=''>--Select--</option>");
         $(arr).each(function (i) {//to list cities
             $("#ciudad").append("<option value=\"" + arr[i].value + "\">" + arr[i].display + "</option>")
         });
